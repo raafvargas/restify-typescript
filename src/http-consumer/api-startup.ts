34b50e1fb5dc68d4e4bc92a0f1707de4cd23b1c4
Rafael Vargas <rafael.vargas@dotz.com.br>
2017-09-06 08:15:02 -0300

    Swagger suppport

--- src/http-consumer/api-startup.ts
+++ src/http-consumer/api-startup.ts
@@ -1,15 +1,17 @@
 import * as restify from 'restify';
+import * as fs from 'fs';
 import { Env } from '../infrastructure/env';
+import * as swagger from 'restify-swagger-jsdoc';
 import { IController } from './base/controller.interface';
 import { HomeController } from './controllers/home.controller';
 import { IStartup } from '../infrastructure/initializable.interface';
-import { RepositoriesController } from './controllers/repositories.constroller';
+import { RepositoriesController } from './controllers/repositories.controller';
 
 export class ApiStartup implements IStartup {
     public name = 'restful-api';
     public restifyServer: restify.Server;
 
-    Run(): Promise<any> {
+    async Run(): Promise<any> {
         this.restifyServer = restify.createServer();
         this.restifyServer.use(restify.plugins.acceptParser(this.restifyServer.acceptable));
         this.restifyServer.use(restify.plugins.authorizationParser());
@@ -21,6 +23,18 @@ export class ApiStartup implements IStartup {
 
         this.controllers().forEach(controller => {
             controller.register(this.restifyServer);
+        });
+        
+        const text = fs.readFileSync(__dirname + '\\swagger.json').toString();
+        const json = JSON.parse(text);
+
+        swagger.createSwaggerPage({
+            title: 'Example API Docs',
+            version: '1.0.0',
+            server: this.restifyServer,
+            path: '/swagger',
+            definitions: json.definitions,
+            forceSecure: false
         });
 
         return new Promise((resolve, reject) => {
